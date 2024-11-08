444aea79d9f3c06a0629c58bae61cdf0248c7028
Chandrashekhar gowda KS <chandrashekhargawda2000@gmail.com>
2024-11-08 10:16:04 +0530

    spline robot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Spline from "@splinetool/react-spline";
+import "./App.css";
+
+function Box(props) {
+  return (
+    <mesh {...props} receiveShadow castShadow>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Main section with split layout */}
+      <section className="split-section">
+        {/* Added heading */}
+        <div className="left-div">
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <p>asdfafadsfdasf asdasd</p>
+        </div>
+
+        <div className="content-right">
+          <div className="spline-container">
+            {/* <Spline scene="https://prod.spline.design/dB8UdZs-XfBTebFa/scene.splinecode" /> */}
+            {/* <Spline scene="https://prod.spline.design/g2HUve91PgXBed5F/scene.splinecode" /> */}
+
+            <Spline scene="https://prod.spline.design/dB8UdZs-XfBTebFa/scene.splinecode" />
+            {/* <Spline scene="https://prod.spline.design/g2HUve91PgXBed5F/scene.splinecode" /> */}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
